b21d3fc1fa9d5dbb0d1ed5e6093af036325ecf57
John Lester Alba <palagpatski@aegir.ld>
2014-12-01 14:42:59 +0800

    Added responsive menu

---
+++ mobile-nav.js
@@ -0,0 +1,11 @@
+(function ($) {
+  Drupal.behaviors.mhcc2 = {
+    attach: function (context, settings) {
+      
+	$('.responsive-nav-toggle').click(function(){
+	  $('.art-hmenu').slideToggle();
+	});
+
+    }
+  };
+})(jQuery);
