581a0cea67cec05ebb19f7d964a9e317a0c066df
John Lester Alba <palagpatski@aegir.ld>
2014-12-08 20:59:24 +0800

    Added script to expand submenus at responsive sizes. Not yet finished.

--- mhcc.js
+++ mhcc.js
@@ -3,7 +3,30 @@
     attach: function (context, settings) {
       
 		$('.responsive-nav-toggle').click(function(){
-		  $('.art-hmenu').slideToggle();
+			$('.art-hmenu').slideToggle();
+		});
+
+		//After loading and resizing events..
+		$(window).on('resize load',function(){
+			//Check if the window width is less than the specified value
+			if ($(window).width() < 678) {
+				//Make sure this element doesn't already exist
+				if (!($(".menu-has-child").length)) {
+					//Find all menu elements which has a submenu
+					$(".region-navigation ul>li").has("ul")
+					//Find their hyperlinks and insert this button after each one.
+					.children('a').after('<button class="menu-has-child"> + </button>');
+				} 
+			} 
+			//Else, if window is greater than specified value..
+			else {
+				//Remove the added buttons
+				$('.menu-has-child').remove();
+			}
+
+			$('.menu-has-child').click(function(){
+				$(this).siblings('.menu').slideToggle();
+			});
 		});
 
     }
