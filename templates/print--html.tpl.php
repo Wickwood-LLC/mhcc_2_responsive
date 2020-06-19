23360afc91775388001edb58ba6468e401c3b2d5
palagpatski <palagpatski@gmail.com>
2020-06-19 15:31:41 +0800

    Adding body classes the print template

--- templates/print--html.tpl.php
+++ templates/print--html.tpl.php
@@ -83,7 +83,7 @@
     <?php endif; ?>
     <?php print $css; ?>
   </head>
-  <body>
+  <body class="<?php print $classes; ?>" <?php print $attributes;?>>
     <?php if (!empty($message)): ?>
       <div class="print-message"><?php print $message; ?></div><p />
     <?php endif; ?>
